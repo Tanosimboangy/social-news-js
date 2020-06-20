c9243eef9ce1c2cbc95368b7c61fbdb0ad162869
Tanosimboangy <jacquit.aja@onja.org>
2020-06-20 09:11:14 +0300

    changing the const for the adding new links into let

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// This is my object which contains all of the list of the links
+// This is my object which contains all of the list of the links.
 let linkList = [
     {
         title: "Facebook",
@@ -21,7 +21,8 @@ let linkList = [
         author: "Brod Lari",
     },
 ];
-// This is the input for the user to enter the option
+
+// This is the input for the user to enter the option.
 let menuLinks = `Choose an option:
 1 : Show links
 2 : Add a link
@@ -30,21 +31,26 @@ let menuLinks = `Choose an option:
 `
 let menuOption = Number(prompt(menuLinks));
 
+// This is a function for the showing lists.
 const listOptions = () => {
     for (let i = 0; i < linkList.length; i++) {
         let listOption = `Title: ${linkList[i].title} \n URL : ${linkList[i].Url} \n Author : ${linkList[i].author}`;
         alert (listOption);
     }
-}
-const addingLink = () => {
-    const newLinkTitle = prompt("Enter the title of the new link");
-    const newLinkURL = prompt("Enter the URL of the new link");
-    const newLinkauthor = prompt("Enter the author of the new link");
+};
 
+// Setting up the function for the adding new link.
+const addingLink = () => {
+    let newLinkTitle = prompt("Enter the title of the new link");
+    let newLinkURL = prompt("Enter the URL of the new link");
+    let newLinkauthor = prompt("Enter the author of the new link");
+
+// Check if the new link does start with (`https://`), if so, this codition will add one in front.
     if (!newLinkURL.startsWith("http://") && !newLinkURL.startsWith("https://")) {
         newLinkURL = `https://${newLinkURL}`;
-        }
+    }
 
+// The new object for user that helps the user for the new object.
     let newObject = {
         title: newLinkTitle,
         Url: newLinkURL,
@@ -52,14 +58,18 @@ const addingLink = () => {
     }
 
     linkList.push(newObject);
-}
+    return;
+};
+
+// A function for the deleting part.
 const deleteLink = () => {
     let indexDelete = Number(
         prompt(`Enter the index of the link to be removed(Between 1 and ${linkList.length})`));
     let deletelink = linkList.splice(indexDelete--, 1);
     alert(`You deleted ${deletelink[indexDelete].title}\n ${deletelink[indexDelete].Url}\n ${deletelink[indexDelete].author}`);
-}
+};
 
+// While loop for the whole condition.
 while (menuOption !== 0) {
     if (menuOption === 1) {
         listOptions();
